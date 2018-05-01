ec53635d9974704fafdd40604d618c0eaef23401
Gabriel-Cordeiro <gabrielsilva.ipms@gmail.com>
2018-05-01 17:23:59 -0300

    Retirado pasta não utilizadas do projeto

--- LanchesWeb/dist/routes/home.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-module.exports = function (app) {
-    app.get('/', function (req, res) {
-        app.src.app.controllers.home.index(app, req, res);
-    });
-};
-//# sourceMappingURL=home.js.map
